d577c3c611f52e5c2fac366b91ee6f2127be18c6
Rau Alexey <rauleha2729@gmail.com>
2025-02-26 16:20:53 +0600

    feat: Добавлен начальный экран

---
+++ StudyFlow/study_flow/lib/pages/subjects_page/subjects_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SubjectsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Предметы'),
+      ),
+      body: Center(
+        child: Text('Здесь будут предметы'),
+      ),
+    );
+  }
+}
